313341f08f4e26f06bcab70ce33ba9d249f8c1fc
kakeru <pgtreize@gmail.com>
2020-08-04 12:46:06 +0900

    UV Save

---
+++ UV.hlsl
@@ -0,0 +1,36 @@
+void PolarCoordinates(float2 UV, float2 Center, float RadialScale, float LengthScale, out float2 Out)
+{
+    float2 delta = UV - Center;
+    float radius = length(delta) * 2 * RadialScale;
+    float angle = atan2(delta.x, delta.y) * 1.0 / 6.28 * LengthScale;
+    Out = float2(radius, angle);
+}
+void RadialShear(float2 UV, float2 Center, float2 Strength, float2 Offset, out float2 Out)
+{
+    float2 delta = UV - Center;
+    float delta2 = dot(delta.xy, delta.xy);
+    float2 delta_offset = delta2 * Strength;
+    Out = UV + float2(delta.y, -delta.x) * delta_offset + Offset;
+}
+void Spherize(float2 UV, float2 Center, float Strength, float2 Offset, out float2 Out)
+{
+    float2 delta = UV - Center;
+    float delta2 = dot(delta.xy, delta.xy);
+    float delta4 = delta2 * delta2;
+    float2 delta_offset = delta4 * Strength;
+    Out = UV + delta * delta_offset + Offset;
+}
+void TilingAndOffset(float2 UV, float2 Tiling, float2 Offset, out float2 Out)
+{
+    Out = UV * Tiling + Offset;
+}
+
+
+void Twirl(float2 UV, float2 Center, float Strength, float2 Offset, out float2 Out)
+{
+    float2 delta = UV - Center;
+    float angle = Strength * length(delta);
+    float x = cos(angle) * delta.x - sin(angle) * delta.y;
+    float y = sin(angle) * delta.x + cos(angle) * delta.y;
+    Out = float2(x + Center.x + Offset.x, y + Center.y + Offset.y);
+}
